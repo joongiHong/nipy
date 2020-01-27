ff411bb0790c9d158ed6ece8261f01b9ad70b4dc
Hong Joongi <joongi1978@naver.com>
2020-01-27 22:24:59 +0900

    급식 정보 딕셔너리 출력 추가 및 리드미 수정
    한달치 급식 정보 저장 기능에 딕셔너리 출력 기능을 추가함. 따라서 리드미를 수정함.

--- nipy.py
+++ nipy.py
@@ -298,6 +298,25 @@ class Smeal:  # Smeal 클래스 생성
                 f.close()  # csv 닫기
                 return("CSV ERROR")
 
+        if output == "d":  # 딕셔너리 저장 기능
+            dict_meal = {}
+
+            try:  # 예외 처리를 위한 try문
+                for i in range(1, last_day + 1):  # 한달치 모두 대입하는 반복문
+                    if i < 10:  # 일 정보가 10 미만일때
+                        i = str(i)
+                        i = "0" + i  # 매개변수 입력 규칙에 의거 두자리로 변환
+
+                    i = str(i)
+
+                    meal = self.day(yeon, dal, i, kind)  # 급식 정보 불러옴
+                    dict_meal[i] = meal  # 딕셔너리 저장
+
+                return(dict_meal)
+
+            except:  # 실패시 실패 에러 안내
+                return("DICT ERROR")
+
 
 # ~~ 3. 학교 학사일정을 불러오는 api ~~
 
@@ -322,41 +341,45 @@ class Scalendar:
         if len(yeon) != 4 or len(dal) != 2 or len(self.sclass) != 1:  # 길이가 알맞는지 확인하는 코드
             return("SIZE ERROR")
 
-        url = "http://stu." + self.ooe + "/sts_sci_sf01_001.do"  # 월간 계획 주소
-        para = {
-            "schulCode": self.code,  # 학교코드
-            "schulCrseScCode": self.sclass,  # 교급
-            "schulKndScCode": "0" + self.sclass,  # 교급
-            "ay": yeon,  # 조회년도
-            "mm": dal  # 조회월
-        }
+        try:
+            url = "http://stu." + self.ooe + "/sts_sci_sf01_001.do"  # 월간 계획 주소
+            para = {
+                "schulCode": self.code,  # 학교코드
+                "schulCrseScCode": self.sclass,  # 교급
+                "schulKndScCode": "0" + self.sclass,  # 교급
+                "ay": yeon,  # 조회년도
+                "mm": dal  # 조회월
+            }
 
-        re = requests.get(url, params=para)  # 나이스 학사일정 조회
-        if int(re.status_code) != 200:  # 비정상 접속시 에러 반환
-            return("SERVER ERROR")
+            re = requests.get(url, params=para)  # 나이스 학사일정 조회
+            if int(re.status_code) != 200:  # 비정상 접속시 에러 반환
+                return("SERVER ERROR")
 
-        html = BeautifulSoup(re.text, 'html.parser')  # 파싱 준비
-        html = html.find_all('td')  # td(테이블 구분) 태그만 불러옴
+            html = BeautifulSoup(re.text, 'html.parser')  # 파싱 준비
+            html = html.find_all('td')  # td(테이블 구분) 태그만 불러옴
 
-        html_size = len(html)  # 몇줄 있는지 확인
-        calendar = {}
+            html_size = len(html)  # 몇줄 있는지 확인
+            calendar = {}
 
-        for i in range(0, html_size):  # 날짜 및 일정 내용 분류
-            html_date = str(html[i].find('em'))  # 날짜 정보 빼오기
-            html_body = str(html[i].find('strong'))  # 일정 정보 빼오기
+            for i in range(0, html_size):  # 날짜 및 일정 내용 분류
+                html_date = str(html[i].find('em'))  # 날짜 정보 빼오기
+                html_body = str(html[i].find('strong'))  # 일정 정보 빼오기
 
-            for sakje in ['<em>', '</em>', '<em class="point2">']:
-                html_date = html_date.replace(sakje, '')  # html 태그 제거
+                for sakje in ['<em>', '</em>', '<em class="point2">']:
+                    html_date = html_date.replace(sakje, '')  # html 태그 제거
 
-            for sakje in ['<strong>', '</strong>']:
-                html_body = html_body.replace(sakje, '')  # html 태그 제거
+                for sakje in ['<strong>', '</strong>']:
+                    html_body = html_body.replace(sakje, '')  # html 태그 제거
 
-            if html_body == "None":  # 학사일정 미 존재시
-                html_body = "학사일정이 존재하지 않습니다."  # 안내멘트
+                if html_body == "None":  # 학사일정 미 존재시
+                    html_body = "학사일정이 존재하지 않습니다."  # 안내멘트
 
-            if html_date == "":  # 날짜정보 미 존재시
-                continue  # 반복문 탈출
+                if html_date == "":  # 날짜정보 미 존재시
+                    continue  # 반복문 탈출
 
-            calendar[html_date] = html_body  # 딕셔너리 추가
+                calendar[html_date] = html_body  # 딕셔너리 추가
+
+        except:
+            calendar = "NO DATABASE"  # 에러 전달
 
         return(calendar)  # 반환
